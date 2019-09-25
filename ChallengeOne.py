0f49c9aef9384a35df710bbc5cb3b9df5c72c384
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-09-25 14:42:02 +0600

    Dictionary

--- ChallengeOne.py
+++ ChallengeOne.py
@@ -1,6 +1,20 @@
-# Print a F using *.
+# Check in a patient's info's for a hospital.
 
-piecesOfF = [5, 2, 5, 2, 2]
+import random
 
-for i in piecesOfF:
-    print(i * "*")
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+isNew = input("Are you here for the first time? ")
+
+if isNew.lower() == "yes":
+    isNew = True
+else:
+    isNew = False
+
+if isNew:
+    randomId = random.randint(1, 100)
+    print(f"Dear {name}({age}), your ID number is:"
+          f" {randomId}. Please wait.")
+else:
+    print(f"Dear {name}({age}), you've been registered with "
+          f"your previous ID number. Please wait.")
