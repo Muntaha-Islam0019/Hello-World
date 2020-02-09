4d1ac600a4b0a9b51739385cacc3031f27d5c529
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-02-09 09:47:48 +0600

    Sets

--- Codes/22 - Sets.py
+++ Codes/22 - Sets.py
@@ -4,3 +4,7 @@ a_list = ['some', 'values', 'well', "let's", 'try', 'mixing', 19, 47, 1]
 a_set = set(a_list)
 
 print(a_set)
+
+# Sets can be explicitly created. Values are gonna be autmatically sorted.
+another_set = {3, 2, 4, 5, 1}
+print(another_set)
