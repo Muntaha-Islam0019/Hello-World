9235d7f911103f313530e6652f7ab44122ec448e
Muntaha Islam <muntaha.islam0019@gmail.com>
2020-02-09 09:43:33 +0600

    Sets

---
+++ Codes/22 - Sets.py
@@ -0,0 +1,6 @@
+# Set is auto-sorted & non-repeating data structure in Python, kinda like maps.
+
+a_list = ['some', 'values', 'well', "let's", 'try', 'mixing', 19, 47, 1]
+a_set = set(a_list)
+
+print(a_set)
